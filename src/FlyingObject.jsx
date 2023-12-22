dafe1bd41f36a6db81959aa455a8587e83d13ddb
Michele Memoli <michele.memoli@gmail.com>
2023-12-22 12:40:55 +0000

    feat(shoe): added banking

--- src/FlyingObject.jsx
+++ src/FlyingObject.jsx
@@ -1,31 +1,46 @@
-import React, { useRef } from 'react'
-import { Vector3 } from 'three'
-import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react';
+import { Vector3 } from 'three';
+import { useFrame } from '@react-three/fiber';
 
-export default function FlyingObject({ children }) {
-  const meshRef = useRef()
+export default function FlyingObject({ children, bankStrength = 0.1 }) {
+  const meshRef = useRef();
+  const prevTangent = useRef(new Vector3());
 
   useFrame((state) => {
-    const t = state.clock.getElapsedTime()
+    const t = state.clock.getElapsedTime();
 
     // Circular trajectory
-    const x = Math.sin(t) * 2
-    const y = Math.cos(t * 2) / 2 + 1
-    const z = Math.cos(t) * 2
+    const x = Math.sin(t) * 2;
+    const y = Math.cos(t * 2) / 2 + 1;
+    const z = Math.cos(t) * 2;
 
     // Update position
-    meshRef.current.position.set(x, y, z)
+    meshRef.current.position.set(x, y, z);
 
     // Determine the tangent to the circle at the current position
-    const tangent = new Vector3(Math.cos(t), 0, -Math.sin(t))
+    const tangent = new Vector3(Math.cos(t), -Math.sin(t * 2) / 4, -Math.sin(t)).normalize();
 
-    // Align the cube's forward vector (assuming Z-forward in this case) with the tangent
-    // This makes the cube face the direction it's moving
-    meshRef.current.quaternion.setFromUnitVectors(new Vector3(1, 0, 0), tangent.normalize())
+    // Calculate the normal (banking axis)
+    const normal = new Vector3(0, 1, 0); // Assuming Y-up in your scene
 
-    // Additional rotation to always face 'up'
-    // meshRef.current.rotateOnAxis(new Vector3(1, 0, 0), Math.PI / 2)
-  })
+    // Calculate the binormal
+    const binormal = new Vector3().crossVectors(tangent, normal).normalize();
 
-  return <group ref={meshRef}>{children}</group>
+    // Calculate the angle between the previous and current tangent vectors
+    const angle = Math.acos(prevTangent.current.dot(tangent));
+
+    // Determine the direction of the banking
+    const direction = Math.sign(prevTangent.current.cross(tangent).dot(normal));
+
+    // Set the quaternion of the mesh to align with the tangent and normal vectors
+    meshRef.current.quaternion.setFromUnitVectors(new Vector3(1, 0, 0), tangent);
+
+    // Apply the banking rotation around the binormal axis
+    meshRef.current.rotateOnAxis(binormal, angle * direction * bankStrength);
+
+    // Update the previous tangent
+    prevTangent.current.copy(tangent);
+  });
+
+  return <group ref={meshRef}>{children}</group>;
 }
